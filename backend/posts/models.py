b077554cda45116172b52ac766e114f0a49e2545
nextLine <daniel.attitsogbe02@gmail.com>
2022-02-22 23:04:44 +0000

    add Topic model / backend

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -1,3 +1,6 @@
+from pydoc_data.topics import topics
+from pyexpat import model
+from turtle import title
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -10,6 +13,21 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(null=True)
+
+    class Meta:
+        ordering = ['created_at']
+
+    def __str__(self):
+        return self.title
+
+
+class Topic(models.Model):
+    title = models.CharField(max_length=50)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    related_posts = models.ManyToManyField(Post)
+
+    class Meta:
+        ordering = ['title']
 
     def __str__(self):
         return self.title
