5d78114b88f4a5fe9e5ebdb4162e61a6424db547
nextLine <daniel.attitsogbe02@gmail.com>
2022-02-24 23:08:35 +0000

    update serialisers

--- backend/posts/serializers.py
+++ backend/posts/serializers.py
@@ -1,19 +1,42 @@
 from rest_framework import serializers
 from posts.models import Post, Topic, Comment
 
+
+class CommentSerializer(serializers.ModelSerializer):
+
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field="username"
+    )
+
+    class Meta:
+        model = Comment
+        fields = ["author", "content", "created_at"]
+
+
+class TopicSerializer(serializers.ModelSerializer):
+
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field="username"
+    )
+
+    class Meta:
+        model = Topic
+        fields = ["title", "author", "related_posts"]
+
+
 class PostSerializer(serializers.ModelSerializer):
+    comments = CommentSerializer(many=True)
+
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field="username"
+    )
+
+    #topics = TopicSerializer(many=True, source="title")
+
     class Meta:
         model = Post
         fields = ["title", "content", "author", "topics", "comments", "created_at", "updated_at"]
 
-
-class CommentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Comment
-        fields = ["post", "author", "content", "created_at"]
-
-
-class TopicSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Topic
-        fields = ["title", "author", "related_posts"]
